30fbd2bbff31f161965878a520aeb447cf061f2b
DhruvJain2005 <jaindhruv322@gmail.com>
2026-01-15 14:40:00 +0530

    first practice

---
+++ bridgelab.js
@@ -0,0 +1,22 @@
+console.log("Node js Backend Started"); // // console.log() is used to print messages or values to the console so that programmers can see what is happening inside the program.we cannot see the output on the browser
+
+// VARIABLES 
+
+let name = "Dhruv" // let is a keyword in JavaScript , it is Used to create a variable , the value of a let variable can be changed later, here in this we create a string variable by using ""
+
+
+let age = 20; // in this phrase , in this we declare an integer value , you can see that we cannot use " " this commas so this acts as an integer value 
+
+
+const country="india"; // in this , we declare a constant value that cannot be changed later . it if fixed or we can say that it is constant 
+
+let isStudent = true;  //  in this , we define a boolean variable by using true oe we can use either false also 
+
+
+console.log(name); // this prints the output of variable name
+console.log(age); // this prints the output of variable age
+console.log(country); // this prints the output of variable country
+console.log(isStudent); // this prints the output of variable isStudent
+
+
+

5c716eafb71e70bf4d2acea65a9cb1a391d34479
DhruvJain2005 <jaindhruv322@gmail.com>
2026-01-20 14:14:35 +0530

    created index.js

--- bridgelab.js
+++ bridgelab.js
@@ -8,9 +8,9 @@ let name = "Dhruv" // let is a keyword in JavaScript , it is Used to create a va
 let age = 20; // in this phrase , in this we declare an integer value , you can see that we cannot use " " this commas so this acts as an integer value 
 
 
-const country="india"; // in this , we declare a constant value that cannot be changed later . it if fixed or we can say that it is constant 
+const country="india"; 
 
-let isStudent = true;  //  in this , we define a boolean variable by using true oe we can use either false also 
+let isStudent = true;  
 
 
 console.log(name); // this prints the output of variable name
@@ -18,5 +18,4 @@ console.log(age); // this prints the output of variable age
 console.log(country); // this prints the output of variable country
 console.log(isStudent); // this prints the output of variable isStudent
 
-
-
+let a=[3,5,7,8];
